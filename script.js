99166540baed9fb40db3740fa4e4bae81a7d3a48
Ruth Berkun <67928066+happyRainDrop@users.noreply.github.com>
2021-01-06 16:14:26 -0800

    Score calculating buttons + better layout

--- script.js
+++ script.js
@@ -14,78 +14,81 @@ var selectedBoxesIds = [];
 // Initial box
 var input = document.getElementById("i0");
 input.style.left = "20px";
-input.style.top = "350px";
+input.style.top = "20px";
 input.addEventListener("keyup", submit);
-input.addEventListener("keyup", deleteBox);
-input.addEventListener("keyup", selectBox);
+input.addEventListener("keyup", callRemoveBox);
+input.addEventListener("keyup", callSelectBox);
 
-// Create new box
-// attaches to body
+// Create new box and attaches to div2
 function newBox(event) {
   boxIndex+=1;
   var box = document.createElement("INPUT");
   box.id = "i"+boxIndex;
   box.style.left = "20px";
-  box.style.top = "350px";
+  box.style.top = "20px";
   box.type = "text";
   box.placeholder = "MBTI type";
   box.draggable = "true";
   box.borderWidth = "thin";
-  document.body.appendChild(box);
+  document.getElementById("div2").appendChild(box);
   box2 = document.getElementById("i"+boxIndex);
   boxArray.push(box2);
   box2.addEventListener("keyup", submit);
   box2.addEventListener("dragend", moveBox);
-  box2.addEventListener("keyup", deleteBox);
-  box2.addEventListener("keyup", selectBox);
+  box2.addEventListener("keyup", callRemoveBox);
+  box2.addEventListener("keyup", callSelectBox);
   box2.addEventListener("dblclick", newBox);
 }
 
 /* Move a box on the screen to a different position
    Triggered when box is released from being dragged */
 function moveBox(event) {
-  var x = event.clientX-50-5;
-  var y = event.clientY-25-5; 
+  var div2Top = document.getElementById("div2").getBoundingClientRect().top+window.pageYOffset;
+  var x = event.pageX-50-15;
+  var y = event.pageY-25-15-div2Top;
   var box = document.getElementById(event.target.id);
   box.style.left = x+"px";
   box.style.top = y+"px";
   moveLines(event.target.id, x, y);
 }
 
+/* Redirects to removeBox */
+function callRemoveBox(event) {
+  if (event.keyCode === 27) // Number 27 is the "esc" key on the keyboard
+    removeBox(event.target.id);
+}
+
 /* Delete a box on a screen and in the array
    Also deletes all attached lines
    Triggered when box is clicked and esc is released */
-function deleteBox(event) {
-  var myBoxId = event.target.id;
-  if (event.keyCode === 27) { // Number 27 is the "esc" key on the keyboard
-    var startLinesOfDeleteBox = getStartLines(myBoxId);
-    var endLinesOfDeleteBox = getEndLines(myBoxId);
-    // remove attached lines
-    for (var j = 0; j<startLinesOfDeleteBox.length; j++) 
-      removeLine(startLinesOfDeleteBox[j].getAttribute("id"));
-    for (var k = 0; k<endLinesOfDeleteBox.length; k++) 
-      removeLine(endLinesOfDeleteBox[k].getAttribute("id"));
-    // removes the HTML element
-    document.getElementById(myBoxId).remove(); 
-    var boxArrayIndex = myBoxId.substring(1);
-    // Shift all the boxes that came after the removed box left, renameids accordingly
-    for (var i = parseInt(boxArrayIndex); i<boxArray.length-1; i++) {
-      boxArray[i] = boxArray[i+1];;
-      var oldId = "i"+(i+1);
-      var newId = "i"+i;
-      // Change the name of startBoxId and endBoxId for lines attached to the shifted boxes
-      var startLines = getStartLines(oldId);
-      var endLines = getendLines(oldId);
-      for (var j = 0; j<startLines.length; j++)
-        startLines[j].setAttribute("startBoxId", newId);
-      for (var k = 0; k<endLines.length; k++)
-        endLines[k].setAttribute("endBoxId", newId);  
-      // change box id    
-      document.getElementById(oldId).id=newId;
-    }
-    boxArray.pop(); // get rid of last, now empty, array slot
-    boxIndex--;
+function removeBox(myBoxId) {
+  var startLinesOfDeleteBox = getStartLines(myBoxId);
+  var endLinesOfDeleteBox = getEndLines(myBoxId);
+  // remove attached lines
+  for (var j = 0; j<startLinesOfDeleteBox.length; j++)
+    removeLine(startLinesOfDeleteBox[j].getAttribute("id"));
+  for (var k = 0; k<endLinesOfDeleteBox.length; k++) 
+    removeLine(endLinesOfDeleteBox[k].getAttribute("id"));
+  // removes the HTML element
+  document.getElementById(myBoxId).remove(); 
+  var boxArrayIndex = myBoxId.substring(1);
+  // Shift all the boxes that came after the removed box left, rename ids accordingly
+  for (var i = parseInt(boxArrayIndex); i<boxArray.length-1; i++) {
+    boxArray[i] = boxArray[i+1];
+    var oldId = "i"+(i+1);
+    var newId = "i"+i;
+    // Change the name of startBoxId and endBoxId for lines attached to the shifted boxes
+    var startLines = getStartLines(oldId);
+    var endLines = getEndLines(oldId);
+    for (var j = 0; j<startLines.length; j++)
+      startLines[j].setAttribute("startBoxId", newId);
+    for (var k = 0; k<endLines.length; k++)
+      endLines[k].setAttribute("endBoxId", newId);  
+    // change box id    
+    document.getElementById(oldId).id=newId;
   }
+  boxArray.pop(); // get rid of last, now empty, array slot
+  boxIndex--;
 }
 
 /* Set the type (and color) for a box
@@ -105,18 +108,32 @@ function submit(event) {
 
 ////////   2. LINE MANIPUTLATION
 
+/* Redirects to selectBox */
+function callSelectBox(event) {
+  var typeBox = document.getElementById(event.target.id);
+  if (event.keyCode === 17 && getQuadra(typeBox.value)!=0) // Number 17 is the "ctrl" key
+    selectBox(event.target.id);
+}
+
 /* Select a box to attach a line to
    Triggered when box is clicked and ctrl is released */
-function selectBox(event) {
-  typeBox = document.getElementById(event.target.id);
-  if (event.keyCode === 17 && getQuadra(typeBox.value)!=0) { // Number 17 is the "ctrl" key
-    document.getElementById(event.target.id).style.borderWidth = "thick";
-    selectedBoxesIds.push(event.target.id);
-    if (selectedBoxesIds.length===2) {
+function selectBox(boxId) {
+  document.getElementById(boxId).style.borderWidth = "thick";
+  selectedBoxesIds.push(boxId);
+  if (selectedBoxesIds.length===2) {
+    // cannot draw line between one box
+    if (selectedBoxesIds[0]!==selectedBoxesIds[1]) 
       createLine(selectedBoxesIds[0], selectedBoxesIds[1]);
-      selectedBoxesIds = []; 
-    }
+    // unselect box if box selected twice in a row
+    else document.getElementById(selectedBoxesIds[0]).style.borderWidth = "thin";
+    selectedBoxesIds = []; 
+  } else if (document.getElementById("output").innerHTML == "Select box (with ctrl,as usual)") {
+    attachAllStartLines(boxId);
+    document.getElementById("output").innerHTML = "Average rank of relations for an "+document.getElementById(boxId).value+" in this group is "+getAverageScoreForOnePerson(boxId).toFixed(3);
+    document.getElementById(boxId).style.borderWidth = "thin";
+    selectedBoxesIds = []; 
   }
+
 }
 
 // Draws a line representing the relation between the two selected types and creates corresponding tooltip
@@ -136,6 +153,7 @@ function createLine(id1, id2) {
   var svgElem = document.getElementById("svg1");
   var line = document.createElementNS("http://www.w3.org/2000/svg","line");
   line.setAttributeNS(null, "id", "l"+lineIndex);
+  line.setAttributeNS(null, "class", "tooltip");
   line.setAttributeNS(null, "startBoxId", id1);
   line.setAttributeNS(null, "endBoxId", id2);
   line.setAttributeNS(null, "x1", x1);
@@ -147,7 +165,7 @@ function createLine(id1, id2) {
   if (lineArray.length===0) svgElem.appendChild(line);
   else svgElem.insertBefore(line, lineArray[lineArray.length-1]);
   // add event listener (for deletion)
-  line.addEventListener("click", deleteLine);
+  line.addEventListener("click", callRemoveLine);
   // add tooltip
   var title = document.createElementNS("http://www.w3.org/2000/svg","title");
   title.id = "t"+lineIndex;
@@ -164,7 +182,7 @@ function createLine(id1, id2) {
 
 /* Redirects to removeLine
    Triggered when line clicked on */
-function deleteLine(event) {
+function callRemoveLine(event) {
   var lineId = event.target.id;
   removeLine(lineId);
 }
@@ -173,12 +191,8 @@ function deleteLine(event) {
    Also deletes corresponding title
    Shifts line array accordingly */
 function removeLine(lineId) {
-  var removeIndex = 0;
+  var removeIndex = parseInt(lineId.substring(1));
   var lineString = "";
-  for (var i = 0; i<lineArray.length; i++) {
-    lineString+=lineArray[i].id+" ";
-    if (lineArray[i].getAttribute("id")===lineId) removeIndex = i;
-  }
   // shift the rest of the lines, change ids accordingly
   document.getElementById(lineId).remove();
   for (var i = removeIndex; i<lineArray.length-1; i++) {
@@ -196,8 +210,8 @@ function removeLine(lineId) {
 function moveLines(boxId, newX, newY) {
   for (var i = 0; i<lineArray.length; i++) {
     var line = lineArray[i];
-    var xNew = parseInt(newX)+50;
-    var yNew = parseInt(newY)+25;
+    var xNew = parseInt(newX)+50+15;
+    var yNew = parseInt(newY)+25+15;
     if (line.getAttribute("startBoxId")===boxId) {
       line.setAttribute("x1", xNew);
       line.setAttribute("y1", yNew);
@@ -235,8 +249,8 @@ function changeLineColors(boxId) {
       }
     }
   } else {
-    var startLines = getStartLines(myBoxId);
-    var endLines = getEndLines(myBoxId);
+    var startLines = getStartLines(boxId);
+    var endLines = getEndLines(boxId);
     // remove attached lines
     for (var j = 0; j<startLines.length; j++) 
       removeLine(startLines[j].getAttribute("id"));
@@ -263,7 +277,94 @@ function getEndLines(boxId) {
   return arr;
 }
 
-////////   3. MBTI info for UI (not needed by Type or Calculator classes)
+////////   3. Button functions
+
+/* Deletes all boxes */
+function removeAllBoxes() {
+  for (var i = 0; i<boxArray.length; i++) {
+    removeBox(boxArray[i].id);
+    i--;
+  }
+  boxArray = [];
+  boxIndex = -1;
+}
+
+/* Removes all lines */
+function removeAllLines() {
+  for (var i = 0; i<lineArray.length; i++) {
+    removeLine(lineArray[i].getAttribute("id"));
+    i--;
+  }
+  lineArray = [];
+  lineIndex = 0;
+}
+
+/* Draws all lines */
+function drawAllLines() {
+  removeAllLines();
+  for (var i = 0; i<boxArray.length; i++) {
+    for (var j = 0; j<boxArray.length; j++) {
+        selectBox(boxArray[i].id);
+        selectBox(boxArray[j].id);
+    }
+  }
+}
+
+/* Deletes all lines and boxes */
+function reset(event) {
+  removeAllLines();
+  removeAllBoxes();
+  document.getElementById("output").innerHTML="Output text for score buttons goes here";
+}
+
+/* Returns average of the ranks of the relations of the selected person */
+function getAverageScoreForOnePerson(personBoxId) {
+  var startLines = getStartLines(personBoxId);
+  var sum = 0;
+  var num = startLines.length;
+  for (var i = 0; i<num; i++) {
+    var index = startLines[i].getAttribute("id").substring(1);
+    var lineTitle = titleArray[index].textContent;
+    var rank = parseInt(lineTitle.substring(lineTitle.length-2));
+    sum+=rank;
+  }
+  return sum/num;
+}
+
+/* Returns average of the ranks of the relations of the whole group */
+function getAverageScoreForGroup() {
+  drawAllLines();
+  var sum = 0;
+  var num = lineArray.length;
+  for (var i = 0; i<boxArray.length; i++) {
+    sum+=getAverageScoreForOnePerson(boxArray[i].id)*(boxArray.length-1);
+  }
+  var avg = sum/num;
+  document.getElementById("output").innerHTML = "Average rank of relations in this group is "+avg.toFixed(3);
+}
+
+// Allows user to select a box for scoring
+function scoreOne() {
+  // deselect all boxes
+  for (var i = 0; i<selectedBoxesIds.length; i++) 
+    document.getElementById(selectedBoxesIds[i]).style.borderWidth = "thin";
+  selectedBoxesIds = [];
+  document.getElementById("output").innerHTML = "Select box (with ctrl,as usual)";
+}
+
+// Attaches start lines to one box
+function attachAllStartLines(boxId) {
+  var existingStartLines = getStartLines(boxId);
+  var existingStartIndexes = [];
+  for (var i = 0; i<existingStartLines.length; i++) 
+    existingStartIndexes.push(existingStartLines[i].getAttribute("endBoxId").substring(1));
+  for (var i = 0; i<boxArray.length; i++) {
+    if (!existingStartIndexes.includes(boxArray[i].id.substring(1)) && boxArray[i].id.substring(1)!==boxId.substring(1))
+      createLine(boxId, boxArray[i].id);
+  }
+}
+
+////////   4. MBTI info for UI (not needed by Type or Calculator classes)
 
 /* QUADRA KEY
   Alpha (Ne-Si, Ti-Fe): 1 BLUE
